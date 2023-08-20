6f006b341f70b5030d3e211a27fa63ddc4aa6dfa
2Sunderam <sunderamdutta@gmail.com>
2023-08-20 16:29:21 +0530

    updated files

--- Backend/src/createDatabase.js
+++ Backend/src/createDatabase.js
@@ -3,7 +3,8 @@ const subscriberModel = require('./models/subscribers')
 const data = require('./data')
 
 // Connect to DATABASE
-const DATABASE_URL = "mongodb://localhost/subscribers";
+//const DATABASE_URL = "mongodb://localhost/subscribers";
+const DATABASE_URL = "mongodb+srv://Sunderam:rn!JnYZfuep6m2w@cluster0.7f2xnkz.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection
 db.on('error', (err) => console.log(err))
